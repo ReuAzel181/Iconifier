4c5592d47b110ca7a7279f9f18550f8b91005968
RAIZEL0130 <yureasel@gmail.com>
2024-01-25 21:09:36 +0800

    updating files

--- WebRefresher/script.js
+++ WebRefresher/script.js
@@ -1,23 +1,41 @@
-
-function loadAndShowContent(htmlFile, imagePath) {
+function loadAndShowContent(htmlFile, targetId, imagePath) {
     // Fetch the content of the specified HTML file
     fetch(htmlFile)
         .then(response => response.text())
         .then(data => {
-            // Update the hidden content with the fetched HTML
-            let hiddenContent = document.getElementById('hiddenContent');
-            hiddenContent.innerHTML = data;
-            hiddenContent.classList.remove('hidden');
+            let tempContainer = document.createElement('div');
+            tempContainer.innerHTML = data;
+
+            let contentElement = tempContainer.querySelector('.' + targetId);
+
+            if (contentElement) {
+                let hiddenContent = document.getElementById('hiddenContent');
+                hiddenContent.innerHTML = contentElement.innerHTML;
+                hiddenContent.classList.remove('hidden');
+
+                clearTableContainer();
+                showImage(imagePath);
+                showImage(imagePath);
+
+                // Remove 'clicked' class from all rows
+                let allRows = document.querySelectorAll('#table-container table tr');
+                allRows.forEach(row => row.classList.remove('clicked'));
+
+                // Apply the 'clicked' class to the clicked row
+                let clickedRow = document.querySelector('#table-container table tr td:first-child');
+                if (clickedRow) {
+                    clickedRow.parentNode.classList.add('clicked');
+                } else {
+                    console.error('Clicked row not found.');
+                }
+            }
         })
         .catch(error => console.error('Error fetching content:', error));
-
-    // Clear the images in the table-container before showing the image
-    clearTableContainer();
-
-    // Show image twice
-    showImage(imagePath);
-    showImage(imagePath);
 }
+
+
+
+
 
 function showContent(textContent, imagePath) {
     // Display text
